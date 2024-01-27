978265026cc5d91d0c8cffccf876fadd1202bbdd
guDutra <gudutra05@outlook.com>
2024-01-26 22:57:44 -0300

    projeto homepage apple finalizado

---
+++ HomePageApple/js/script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector('#product-image');
+
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+
+        buttons.forEach((btn) => {
+            btn.querySelector('.color').classList.remove('selected');
+        });
+
+        const button = e.target;
+        const id = button.getAttribute('id');
+        
+        button.querySelector(".color").classList.add("selected");
+        image.classList.add('changing');
+        image.setAttribute("src", `img/iphone_${id}.jpg`);
+        setTimeout(() => {
+            image.classList.toggle('changing');
+        },200)
+    })
+})
